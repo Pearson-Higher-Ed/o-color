5aa48f8f8891db3864737c479292132c4a834e81
Adam Armour <adam.armour@gmail.com>
2015-11-10 16:48:22 -0700

    checkpoint: implement to current design spec

--- demos/src/config.js
+++ demos/src/config.js
@@ -5,7 +5,119 @@ module.exports = {
 	"demos": [
 		{
 			"name": "demo",
-			"template": "demos/src/demo.mustache"
+			"template": "demos/src/demo.mustache",
+			"data": {
+				"swatches": [
+					{
+						"palette": "light",
+						"label": "Light palette",
+						"categories": [
+							{
+								"category": "fg",
+								"label": "Foreground",
+								"types": [
+									{
+										"type": "primary",
+										"label": "Primary"
+									},
+									{
+										"type": "secondary",
+										"label": "Secondary"
+									}
+								]
+							},
+							{
+								"category": "bg",
+								"label": "Background",
+								"types": [
+									{
+										"type": "primary-light",
+										"label": "Primary, light"
+									},
+									{
+										"type": "primary",
+										"label": "Primary"
+									},
+									{
+										"type": "primary-dark",
+										"label": "Primary, dark"
+									},
+									{
+										"type": "highlight",
+										"label": "Highlight"
+									}
+								]
+							},
+							{
+								"category": "accent",
+								"label": "Accent",
+								"types": [
+									{
+										"type": "primary",
+										"label": "Primary"
+									},
+									{
+										"type": "secondary",
+										"label": "Secondary"
+									}
+								]
+							}
+						]
+					},
+					{
+						"palette": "dark",
+						"label": "Dark palette",
+						"categories": [
+							{
+								"category": "fg",
+								"label": "Foreground",
+								"types": [
+									{
+										"type": "primary",
+										"label": "Primary"
+									},
+									{
+										"type": "secondary",
+										"label": "Secondary"
+									}
+								]
+							},
+							{
+								"category": "bg",
+								"label": "Background",
+								"types": [
+									{
+										"type": "primary-light",
+										"label": "Primary, light"
+									},
+									{
+										"type": "primary",
+										"label": "Primary"
+									},
+									{
+										"type": "primary-dark",
+										"label": "Primary, dark"
+									}
+								]
+							},
+							{
+								"category": "accent",
+								"label": "Accent",
+								"types": [
+									{
+										"type": "primary",
+										"label": "Primary"
+									},
+									{
+										"type": "secondary",
+										"label": "Secondary"
+									}
+								]
+							}
+						]
+					}
+				]
+			}
 		}
 	]
 };
